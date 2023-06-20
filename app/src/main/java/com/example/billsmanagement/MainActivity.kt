496268ea1851c29700697fc1d27a7974a86243a0
leilaashur <leilahashur@gmail.com>
2023-06-20 10:04:51 +0300

    Bills commit

--- app/src/main/java/com/example/billsmanagement/MainActivity.kt
+++ app/src/main/java/com/example/billsmanagement/MainActivity.kt
@@ -5,25 +5,64 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.billsmanagement.databinding.ActivityMainBinding
 
+
+
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 
     override fun onResume() {
         super.onResume()
+        setContentView(binding.root)
         binding.btnProceed.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
+            validateRegistration()
+        }
+    }
+    fun validateRegistration() {
+        val name = binding.etName.text.toString()
+        val phonenumber =binding.etNumber .text.toString()
+        val email=binding.etEmail .text.toString()
+        val password =binding.etPassword .text.toString()
+
+        var error = false
+        if (name.isBlank()) {
+            binding.etName.error = "Name required"
+            error=true
+        }
+        if (name.isBlank()) {
+            binding.tillNumber.error = "Phonenumber required"
+            error=true
+        }
+        if (name.isBlank()) {
+            binding.tillEmail.error = "Email required"
+            error=true
+        }
+
+        if (name.isBlank()) {
+            binding.tillPassword.error = "Password required"
+            error=true
+        }
+
+        if (!error){
+            val intent= Intent(this,Login::class.java)
             startActivity(intent)
-
-
         }
     }
 }
+
+
+
+
+
+
+
+
 
 
 
